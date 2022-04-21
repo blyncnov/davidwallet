3ce3ac2eac1a3be24fcb74c9a08581069cbb3daf
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-21 22:23:12 +0100

    here david's wallet v26

--- src/components/sucess.jsx
+++ src/components/sucess.jsx
@@ -5,7 +5,7 @@ const Sucess = () => {
         <>
             <br />
             <div className="bar_suc">
-                <h1>Successfully</h1>
+                <h1>Successfully ✅</h1>
                 <br />
                 <img src="https://www.pngitem.com/pimgs/m/461-4615243_qr-code-file-svg-wikimedia-commons-business-card.png" alt="barcxode" />
            </div>
